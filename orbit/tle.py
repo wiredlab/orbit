17dfc2694c0acb119641bcf453fb2486e99e0c0a
Dan White <dan@whiteaudio.com>
2018-06-21 19:18:31 -0500

    raise a KeyError if the satellite is unknown to CelesTrak

--- orbit/tle.py
+++ orbit/tle.py
@@ -9,11 +9,14 @@ from . import utilities
 
 requests_cache.install_cache(expire_after=86400)
 
+
 def get(catnr):
     page = html.fromstring(requests.get('http://www.celestrak.com/cgi-bin/TLE.pl?CATNR=%s' % catnr).text)
     tle = page.xpath('//pre/text()')[0].split('\n')
+    if len(tle) != 5:
+        raise KeyError(' '.join(tle[1:-1]))
     return tle[1].strip(), tle[2].strip(), tle[3].strip()
-    
+
 def parse(name, line1, line2):
     tle_rec = ephem.readtle(name, line1, line2)
     tle_rec.compute()
